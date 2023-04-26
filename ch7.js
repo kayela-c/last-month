7a34c32e6f62f825e351f5159ed2ef2328d28442
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2023-04-26 03:28:44 +0000

    V

--- ch7.js
+++ ch7.js
@@ -30,7 +30,20 @@ function parseScores(scores) {
      function setTableContent(userInput) {
         // TODO: Compete the function
 
-     }
+        const scoresArray = parseScores(scores);
+        const distributionArray = buildDistributionArray(scoresArray);
+        let firstRowHTML = "";
+    
+        for (let i = 0; i < distributionArray.length; i++) {
+            firstRowHTML += `<td><div style="height:${distributionArray[i] * 10}px" class="bar${i}"></div></td>`;
+        }
+    
+        document.getElementById("firstRow").innerHTML = firstRowHTML;
+        document.getElementById("thirdRow").innerHTML = distributionArray.map((count) => `<td>${count}</td>`).join("");
+    
+        
+    
+    }
      
      // The argument can be changed for testing purposes
      setTableContent("45 78 98 83 86 99 90 59");    return scores.split(" ");
@@ -38,19 +51,6 @@ function parseScores(scores) {
 
 
 
-
-function setTableContent(scores) {
-    const scoresArray = parseScores(scores);
-    const distributionArray = buildDistributionArray(scoresArray);
-    let firstRowHTML = "";
-
-    for (let i = 0; i < distributionArray.length; i++) {
-        firstRowHTML += `<td><div style="height:${distributionArray[i] * 10}px" class="bar${i}"></div></td>`;
-    }
-
-    document.getElementById("firstRow").innerHTML = firstRowHTML;
-    document.getElementById("thirdRow").innerHTML = distributionArray.map((count) => `<td>${count}</td>`).join("");
-}
 
 
 
