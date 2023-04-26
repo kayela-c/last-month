eaa72cdf9731c018df958ad293686c3881b4f023
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2023-04-26 03:26:36 +0000

    '

--- ch7.js
+++ ch7.js
@@ -8,35 +8,36 @@ function parseScores(scores) {
      
      function buildDistributionArray(scoresArray) {
         // TODO: Compete the function
+        let distribution = [0, 0, 0, 0, 0];
+
+        for (let score of scoresArray) {
+            if (score >= 90) {
+                distribution[0]++;
+            } else if (score >= 80) {
+                distribution[1]++;
+            } else if (score >= 70) {
+                distribution[2]++;
+            } else if (score >= 60) {
+                distribution[3]++;
+            } else {
+                distribution[4]++;
+            }
+        }
+    
+        return distribution;
      }
      
      function setTableContent(userInput) {
         // TODO: Compete the function
+
      }
      
      // The argument can be changed for testing purposes
      setTableContent("45 78 98 83 86 99 90 59");    return scores.split(" ");
 }
 
-function buildDistributionArray(scoresArray) {
-    let distribution = [0, 0, 0, 0, 0];
 
-    for (let score of scoresArray) {
-        if (score >= 90) {
-            distribution[0]++;
-        } else if (score >= 80) {
-            distribution[1]++;
-        } else if (score >= 70) {
-            distribution[2]++;
-        } else if (score >= 60) {
-            distribution[3]++;
-        } else {
-            distribution[4]++;
-        }
-    }
 
-    return distribution;
-}
 
 function setTableContent(scores) {
     const scoresArray = parseScores(scores);
@@ -51,5 +52,5 @@ function setTableContent(scores) {
     document.getElementById("thirdRow").innerHTML = distributionArray.map((count) => `<td>${count}</td>`).join("");
 }
 
-// Example usage:
-setTableContent("45 78 98 83 86 99 90 59");
+
+
