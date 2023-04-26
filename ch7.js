c2cbb67fee365f022438f593093ce5aaf4fd247e
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2023-04-26 03:19:22 +0000

    Downloaded files

--- ch7.js
+++ ch7.js
@@ -1,5 +1,21 @@
+
+
+
 function parseScores(scores) {
-    return scores.split(" ");
+    function parseScores(scoresString) {
+        // TODO: Compete the function
+     }
+     
+     function buildDistributionArray(scoresArray) {
+        // TODO: Compete the function
+     }
+     
+     function setTableContent(userInput) {
+        // TODO: Compete the function
+     }
+     
+     // The argument can be changed for testing purposes
+     setTableContent("45 78 98 83 86 99 90 59");    return scores.split(" ");
 }
 
 function buildDistributionArray(scoresArray) {
