96c8a0944fbf8f29aa63c6a1c3c7e29e80206aac
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2023-04-30 02:12:45 +0000

    changed

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
-
-
-
-function parseScores(scores) {
-    function parseScores(scoresString) {
-        // TODO: Compete the function
-     }
-     
-     function buildDistributionArray(scoresArray) {
-        // TODO: Compete the function
-        let distribution = [0, 0, 0, 0, 0];
-
-        for (let score of scoresArray) {
-            if (score >= 90) {
-                distribution[0]++;
-            } else if (score >= 80) {
-                distribution[1]++;
-            } else if (score >= 70) {
-                distribution[2]++;
-            } else if (score >= 60) {
-                distribution[3]++;
-            } else {
-                distribution[4]++;
-            }
-        }
-    
-        return distribution;
-     }
-     
-     function setTableContent(userInput) {
-        // TODO: Compete the function
-
-        const scoresArray = parseScores(scores);
-        const distributionArray = buildDistributionArray(scoresArray);
-        let firstRowHTML = "";
-    
-        for (let i = 0; i < distributionArray.length; i++) {
-            firstRowHTML += `<td><div style="height:${distributionArray[i] * 10}px" class="bar${i}"></div></td>`;
-        }
-    
-        document.getElementById("firstRow").innerHTML = firstRowHTML;
-        document.getElementById("thirdRow").innerHTML = distributionArray.map((count) => `<td>${count}</td>`).join("");
-    
-        
-    
-    }
-     
-     // The argument can be changed for testing purposes
-     setTableContent("45 78 98 83 86 99 90 59");    return scores.split(" ");
+function parseScores(scoresString) {
+    return scoresString.split(" ");
 }
 
+function buildDistributionArray(scoresArray) {
+    let distributionArray = [0, 0, 0, 0, 0];
 
+    scoresArray.forEach(score => {
+        let numScore = parseInt(score);
+        if (numScore >= 90) distributionArray[0]++;
+        else if (numScore >= 80) distributionArray[1]++;
+        else if (numScore >= 70) distributionArray[2]++;
+        else if (numScore >= 60) distributionArray[3]++;
+        else distributionArray[4]++;
+    });
 
+    return distributionArray;
+}
 
+function setTableContent(scoresString) {
+    const scoresArray = parseScores(scoresString);
+    const distributionArray = buildDistributionArray(scoresArray);
 
+    let firstRowHTML = "";
+    let thirdRowHTML = "";
+    for (let i = 0; i < distributionArray.length; i++) {
+        const barHeight = distributionArray[i] * 10;
+        firstRowHTML += `<td><div style="height:${barHeight}px" class="bar${i}"></div></td>`;
+        thirdRowHTML += `<td>${distributionArray[i]}</td>`;
+    }
 
+    document.getElementById("firstRow").innerHTML = firstRowHTML;
+    document.getElementById("thirdRow").innerHTML = thirdRowHTML;
+}
+
+// Example usage:
+setTableContent("45 78 98 83 86 99 90 59");
